45c2eb39b2440f72143cc7289219727796086cad
Lev <levdjepko@gmail.com>
2024-04-08 21:14:25 -0700

    Update script.js

--- rpg_game/script.js
+++ rpg_game/script.js
@@ -159,10 +159,12 @@ function goFight() {
   monsterHealthText.innerText = monsterHealth;
 }
 
+
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
 }
 
 function dodge() {
